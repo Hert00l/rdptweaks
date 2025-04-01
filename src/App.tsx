a0f6eaad154fcba10d7c91e9f117330c3beb44ff
Hert00l <fortkebab84@gmail.com>
2025-04-01 19:51:06 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -187,8 +187,8 @@ function App() {
         <nav className="fixed w-full z-50 bg-black/80 backdrop-blur-lg border-b border-red-600/20">
           <div className="container mx-auto px-4">
             <div className="flex items-center justify-between h-20">
-              <a href="https://sigmawire.net/i/04/fWPqNO.png">
-                <img src="https://sigmawire.net/i/04/fWPqNO.png" alt="RDP Tweaks Logo" className="h-24" />
+              <a href="https://sigmawire.net/i/03/xLiVze.png">
+                <img src="https://sigmawire.net/i/03/xLiVze.png" alt="RDP Tweaks Logo" className="h-24" />
               </a>
               
               <button 
@@ -259,7 +259,7 @@ function App() {
               >
                 <div className="absolute inset-0 bg-red-500/30 rounded-xl blur-2xl group-hover:bg-red-500/50 transition-all duration-500" />
                 <img
-                  src="https://sigmawire.net/i/03/MX3QOP.png"
+                  src="hthttps://sigmawire.net/i/04/fWPqNO.png"
                   alt="Gaming Setup Preview"
                   className="relative rounded-xl border border-red-500/20 group-hover:border-red-500/50 transition-all duration-500 transform group-hover:scale-[1.02]"
                 />
